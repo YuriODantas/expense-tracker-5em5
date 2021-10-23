5d4fc698201748953f0174659cb865e261921f29
YuriODantas <iruy3343@gmail.com>
2021-10-23 14:38:31 -0300

    Create the structure for new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import * as C from './App.styles'
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { items } from './data/items';
+import { categories } from './data/categories';
+import { filterListByMonth, getCurrentMonth } from './helpers/dateFilter';
+import { TableArea } from './components/TableArea';
+import { InfoArea } from './components/InfoArea';
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth])
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth)
+  }
+
   return (
     <C.Container>
       <C.Header>
@@ -9,11 +30,16 @@ const App = () => {
       </C.Header>
       <C.Body>
       
-      {/* Área de Informações */}
+     <InfoArea 
+      currentMonth={currentMonth}
+      onMonthChange={handleMonthChange}
+      income={income}
+      expense={expense}
+    />
 
       {/* Área de inserção */}
 
-      {/* Tabela de Itens */}
+      <TableArea list={filteredList} />
 
       </C.Body>
     </C.Container>
